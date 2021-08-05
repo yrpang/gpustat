a5189b4bc649d25aaad818ebe9b79738b7c6e69a
Jongwook Choi <wookayin@gmail.com>
2021-08-05 06:34:05 -0400

    Use NVIDIA's official pynvml binding
    Since 2021, NVIDIA provies an official python binding `pynvml`,
    which should replace existing third-party (not maintained) one.
    The APIs are mostly the same, but it ships with some recent features
    such as MIG support.
    
    To ensure a correct `pynvml` package is installed, we perform
    some sanity check upon importing the library.

--- gpustat/core.py
+++ gpustat/core.py
@@ -19,12 +19,28 @@ import os.path
 import platform
 import sys
 import time
+import textwrap
 from datetime import datetime
 
 from six.moves import cStringIO as StringIO
-import psutil
-import pynvml as N
 from blessed import Terminal
+import psutil
+try:
+    import pynvml as N
+    if not hasattr(N, 'NVML_BRAND_TITAN_RTX'):
+        raise RuntimeError("pynvml library is outdated.")
+except (ImportError, SyntaxError, RuntimeError) as e:
+    raise ImportError(textwrap.dedent(
+        """\
+        pynvml is missing or an outdated version is installed.
+
+        Please reinstall `gpustat`:
+        $ pip install -I gpustat
+
+        or manually fix the package:
+        $ pip uninstall nvidia-ml-py3
+        $ pip install -I 'nvidia-ml-py!=375.*'
+        """)) from e
 
 import gpustat.util as util
 
